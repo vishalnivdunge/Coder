5656dfb36f996133bd52d3e0c87a668ab5a6c27f
Vishal Nivdunge <162100127+vishalnivdunge@users.noreply.github.com>
2026-01-06 13:16:31 +0000

    Operator

---
+++ Basic_program/Arithmatic_.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Arithmatic_ {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the value of a :");
+        int a = sc.nextInt();
+
+        System.out.println("Enter the value of b");
+
+        int b = sc.nextInt();
+int c;
+        System.out.println("addition =" + (a+b)20
+    );
+        System.out.println("subtraction ="+(a-b));
+        System.out.println("multiplication  =" + a*b);
+        System.out.println("dividation=" + a/b);
+        System.out.println("percentage =" + a%b);
+    }
+
+}
